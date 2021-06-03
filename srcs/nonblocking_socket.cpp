018d21289a1b43c92ff22cd1aad765b97e6addd6
David ARBIB <davidarbib@yahoo.fr>
2021-06-03 12:10:25 +0200

    read in non blocking mode works

--- srcs/nonblocking_socket.cpp
+++ srcs/nonblocking_socket.cpp
@@ -4,27 +4,31 @@
 #include <strings.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <vector>
+#include <iostream>
 
 #include <fcntl.h>
 #include <sys/select.h>
 
-#define PORT	80
+#define PORT	8002
 #define BUFSIZE 2000
 #define DELAY	5
 
-int	create_connection()
+int	create_connection(int sock_fd, fd_set *origin_fds, int *maxfd)
 {
 	sockaddr_in new_sin;
 	int			new_sock_fd;
 	socklen_t sinsize = sizeof(new_sin);
 
 	new_sock_fd = accept(sock_fd, reinterpret_cast<sockaddr*>(&new_sin), &sinsize);
+	if (new_sock_fd < 0)
+		perror("socket");
 	printf("New socket fd : %d\n", new_sock_fd);
 	fcntl(new_sock_fd, F_SETFL, O_NONBLOCK);
-	FD_SET(new_sock_fd, &readfds); 
-	FD_SET(new_sock_fd, &writefds); 
-	if (new_sock_fd > maxfd)
-		maxfd = new_sock_fd;
+	FD_SET(new_sock_fd, origin_fds);
+	if (new_sock_fd > *maxfd)
+		*maxfd = new_sock_fd;
+	return new_sock_fd;
 }
 
 int main(int ac, char **av)
@@ -33,12 +37,16 @@ int main(int ac, char **av)
 	(void)av;
 	sockaddr_in sin;
 	int			maxfd;
+	std::vector<int> fds;
 	
-	fd_set readfds;
+	fd_set read_fds;
+	fd_set write_fds;
+	fd_set origin_fds;
 	struct timeval tv;
 
 	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-	maxfd = sockfd;
+	fcntl(sock_fd, F_SETFL, O_NONBLOCK);
+	maxfd = sock_fd;
 
 	tv.tv_sec = DELAY;
 	tv.tv_usec = 0;
@@ -49,34 +57,38 @@ int main(int ac, char **av)
 
 	bind(sock_fd, reinterpret_cast<sockaddr*>(&sin), sizeof(sin));
 	listen(sock_fd, 1);
-	fcntl(sock_fd, F_SETFL, O_NONBLOCK);
-	FD_SET(sock_fd, &readfs);
+	FD_SET(sock_fd, &read_fds);
+	int connection = 0;
 	while (1)
-	{
-		//select
-		//check listen socket
-		//	connection creation
-		//		
-		int ret = select(maxfd + 1, &readfds, &writefds, NULL, &tv);
-		if (FD_ISSET(sock_fd, &readfs))
-			create_connection();
-		if (new_sock_fd > 0)
+	{	
+		if (connection)
+			FD_SET(fds[0], &read_fds);
+		int ret = select(maxfd + 1, &read_fds, &write_fds, NULL, &tv);
+		if (FD_ISSET(sock_fd, &read_fds))
 		{
-			continue;
+			std::cout << "connection" << std::endl;
+			fds.push_back(create_connection(sock_fd, &origin_fds, &maxfd));	
+			connection = 1;
 		}
-		if (!ret)
-			printf("No data within %d seconds\n", DELAY);
+		/*
+		if (ret)
+			// reads / writes
+		*/
 		if (ret > 0)
 		{
-			printf("read socket : %d\n", new_sock_fd);
-			char buf[BUFSIZE];
-			bzero(buf, BUFSIZE);
-			read(new_sock_fd, buf, BUFSIZE);
-			printf("%s\n", buf);
+			if (FD_ISSET(fds[0], &read_fds))
+			{
+				printf("read socket : %d\n", fds[0]);
+				char buf[BUFSIZE];
+				bzero(buf, BUFSIZE);
+				read(fds[0], buf, BUFSIZE);
+				printf("%s\n", buf);
+				FD_CLR(fds[0], &read_fds);
+			}
 		}
-		if (new_sock_fd > 0)
-			close(new_sock_fd);
 	}
+	//close all sockets
+	close(fds[0]);
 	close(sock_fd);
 	return 0;
 }
