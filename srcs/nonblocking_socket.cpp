3e2849f84359ce20031f567b4b180a8276ea90a6
David ARBIB <darbib@e3r11p16.42.fr>
2021-06-07 19:59:55 +0200

    sigint handling to close sockets

--- srcs/nonblocking_socket.cpp
+++ srcs/nonblocking_socket.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
+#include <csignal>
 
 #include <fcntl.h>
 #include <sys/select.h>
@@ -13,6 +14,15 @@
 #define PORT	8003
 #define BUFSIZE 2000
 #define DELAY	5
+
+int g_close_flag = 0;
+
+void signalHandler(int signum)
+{
+	g_close_flag = 1;
+	std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
+	exit(1);
+}
 
 int	create_connection(int sock_fd, fd_set *origin_fds, int *maxfd)
 {
@@ -46,6 +56,7 @@ int main(int ac, char **av)
 
 	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	fcntl(sock_fd, F_SETFL, O_NONBLOCK);
+	std::cout << "sock fd at beginning : " << sock_fd << std::endl;
 	maxfd = sock_fd;
 
 	tv.tv_sec = DELAY;
@@ -55,23 +66,32 @@ int main(int ac, char **av)
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(PORT);
 
-	bind(sock_fd, reinterpret_cast<sockaddr*>(&sin), sizeof(sin));
+	int retbind = bind(sock_fd, reinterpret_cast<sockaddr*>(&sin), sizeof(sin));
+	if (retbind < 0)
+	{
+		perror("webserv");
+		exit(EXIT_FAILURE);
+	}
 	listen(sock_fd, 1);
 	int writeyet = 0;
+	FD_ZERO(&read_fds);
+	FD_ZERO(&write_fds);
+	signal(SIGINT, signalHandler);
 	while (1)
-	{	
+	{
 		FD_SET(sock_fd, &read_fds);
-		printf("%ld\n", read_fds.fds_bits[0]);
+		//std::cout << read_fds.fds_bits[0] << std::endl;
 		if (fds.size() > 0)
 		{
 			FD_SET(fds[0], &read_fds);
 			FD_SET(fds[0], &write_fds);
 		}
 		int ret = select(maxfd + 1, &read_fds, &write_fds, NULL, &tv);
-		if (FD_ISSET(sock_fd, &read_fds))
+		if (FD_ISSET(sock_fd, &read_fds) && !g_close_flag)
 		{
 			std::cout << "connection" << std::endl;
 			fds.push_back(create_connection(sock_fd, &origin_fds, &maxfd));	
+			FD_CLR(sock_fd, &read_fds);
 		}
 		/*
 		if (ret)
@@ -79,7 +99,7 @@ int main(int ac, char **av)
 		*/
 		if (fds.size() > 0 && ret > 0)
 		{
-			if (FD_ISSET(fds[0], &read_fds))
+			if (FD_ISSET(fds[0], &read_fds) && !g_close_flag)
 			{
 				//printf("read socket : %d\n", fds[0]);
 				char buf[BUFSIZE];
@@ -102,9 +122,13 @@ int main(int ac, char **av)
 				writeyet = 1;
 			}
 		}
+		if (g_close_flag)
+			break;
 	}
 	//close all sockets
-	close(fds[0]);
+	if (fds.size() > 0)
+		close(fds[0]);
+	std::cout << "sock fd at close: " << sock_fd << std::endl;
 	close(sock_fd);
 	return 0;
 }
