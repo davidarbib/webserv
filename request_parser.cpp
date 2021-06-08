777913f274b4148ee0d4778ab99959b6cb053f7f
macrespo42 <maxime.crespopro@gmail.com>
2021-06-08 15:52:16 +0200

    implement request_URI parsing

--- request_parser.cpp
+++ request_parser.cpp
@@ -1,6 +1,7 @@
 #include "Request.hpp"
 
-bool	is_end_line(char *line, int index)
+bool
+is_end_line(char *line, int index)
 {
 	if (line[index] == '\r')
 	{
@@ -10,7 +11,8 @@ bool	is_end_line(char *line, int index)
 	return false;
 }
 
-bool	is_end_section(char *line, int index)
+bool
+is_end_section(char *line, int index)
 {
 	if (is_end_line(line, index))
 	{
@@ -20,7 +22,8 @@ bool	is_end_section(char *line, int index)
 	return false;
 }
 
-int		parse_method_token(const char *raw_request, Request *request)
+int
+parse_method_token(const char *raw_request, Request *request)
 {
 	int index = 0;
 	std::string method_token;
@@ -31,12 +34,29 @@ int		parse_method_token(const char *raw_request, Request *request)
 		index++;
 	}
 	request->set_method_token(method_token);
-	return index;
+	return index + 1;
 }
 
-void	parse_request(char *raw_request, Request *request)
+int
+parse_request_URI(const char *raw_request, Request *request, int position)
+{
+	int index = position;
+	std::string request_URI;
+
+	while (raw_request[index] && raw_request[index] != ' ')
+	{
+		request_URI += raw_request[index];
+		index++;
+	}
+	request->set_request_URI(request_URI);
+	return index + 1;
+}
+
+void
+parse_request(char *raw_request, Request *request)
 {
 	int request_position = 0;
 
 	request_position = parse_method_token(raw_request, request);
+	request_position = parse_request_URI(raw_request, request, request_position);
 }
