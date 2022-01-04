3a4fb8c25c9a2fbda7959a9c23d1c2b1c9520545
David ARBIB <davidarbib@yahoo.fr>
2022-01-04 19:51:07 +0100

    string ip and port stored in Connection class, todo integrate with requesthandler

--- includes/Connection.hpp
+++ includes/Connection.hpp
@@ -4,6 +4,7 @@
 # include <string>
 # include <iostream>
 # include "typedefs.hpp"
+# include <sstream>
 
 class Server;
 
@@ -15,19 +16,47 @@ class Connection
 		Connection(Connection const &src);
 		~Connection(void);
 
-		fd_t			getSocketFd(void) const;
-		unsigned long	getClientIp(void) const;
-		std::string		getClientIpStr(void) const;
-		unsigned short	getClientPort(void) const;
-		std::string		getClientPortStr(void) const;
-		Server			&getServer(void) const;
+		/*
+		** getters return ip and port in host format
+		** or string form
+		*/
+
+		fd_t
+		getSocketFd(void) const;
+
+		unsigned long
+		getClientIp(void) const;
+
+		std::string
+		getClientIpStr(void) const;
+
+		unsigned short
+		getClientPort(void) const;
+
+		std::string
+		getClientPortStr(void) const;
+
+		Server&
+		getServer(void) const;
 
 	private:
+		/*
+		** ! _client_ip and _client_port are in network format
+		*/
 		fd_t			_socket_fd;
 		unsigned long	_client_ip;
 		unsigned short	_client_port;
+		std::string		_client_ip_str;
+		std::string		_client_port_str;
 		Server			&_server;
+
 		Connection(void);
+
+		void
+		makeIpStr(void);
+
+		void
+		makePortStr(void);
 
 };
 
--- srcs/Connection.cpp
+++ srcs/Connection.cpp
@@ -4,7 +4,10 @@ Connection::Connection(fd_t fd, unsigned long client_ip, unsigned short client_p
 						Server &server)
 : _socket_fd(fd), _client_ip(client_ip), _client_port(client_port),
 	_server(server)
-{ }
+{
+	makeIpStr();
+	makePortStr();
+}
 
 Connection::Connection(Connection const &src)
 : _socket_fd(src._socket_fd), _client_ip(src._client_ip),
@@ -20,6 +23,8 @@ Connection::operator=(Connection const &rhs)
 	_socket_fd = rhs._socket_fd;
 	_client_ip = rhs._client_ip;
 	_client_port = rhs._client_port;
+	_client_ip_str = rhs._client_ip_str;
+	_client_port_str = rhs._client_port_str;
 	_server = rhs._server;
 	return *this;
 }
@@ -30,12 +35,36 @@ Connection::getSocketFd(void) const
 
 unsigned long		
 Connection::getClientIp(void) const
-{ return _client_ip; }
+{ return ntohl(_client_ip); }
 
 unsigned short
 Connection::getClientPort(void) const
-{ return _client_port; }
+{ return ntohs(_client_port); }
+
+std::string
+Connection::getClientIpStr(void) const
+{ return _client_ip_str; }
+
+std::string
+Connection::getClientPortStr(void) const
+{ return _client_port_str; }
 
 Server &
 Connection::getServer(void) const
 { return _server; }
+
+void
+Connection::makeIpStr(void)
+{
+	char ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &_client_ip, ip, INET_ADDRSTRLEN);
+	_client_ip_str = std::string(ip);
+}
+
+void
+Connection::makePortStr(void)
+{
+	std::stringstream stream;
+	stream << ntohs(_client_port);
+	stream >> _client_port_str;
+}
