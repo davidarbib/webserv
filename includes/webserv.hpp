fdc044d91c1f3cdd448b56961c349b17412b8c3d
David ARBIB <davidarbib@yahoo.fr>
2022-01-10 12:14:59 +0100

    gitignore no longer ignore webserv pattern in subfolders

---
+++ includes/webserv.hpp
@@ -0,0 +1,11 @@
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+	
+# include "Server.hpp"
+# include "request_parser.hpp"
+# include "RequestHandler.hpp"
+# include "Ticket.hpp"
+# include <deque>
+# include <queue>
+
+#endif
