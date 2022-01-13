2f025aa0d71a7e715793cd702326929711d16cf3
lnezonde <34407290+lnezonde@users.noreply.github.com>
2022-01-13 16:08:08 +0100

    added config files

---
+++ includes/ConfigUtils.hpp
@@ -0,0 +1,24 @@
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <string>
+# include <string.h>
+# include <vector>
+# include <iostream>
+# include <fstream>
+# include <sstream>
+
+
+std::string
+parse(std::string const &confFile, int pos_start);
+
+std::string
+getNextBlock(std::string const &confFile, int pos_start);
+
+std::vector<std::string>
+getBlocks(std::string const &confFile, std::string const &block_name);
+
+std::string
+read_config(std::string config_path);
+
+#endif
