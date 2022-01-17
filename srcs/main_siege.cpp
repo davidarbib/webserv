92a7a652ed06cda5455a172ec7071a0e6437b7b6
David ARBIB <darbib@e2r9p4.clusters.42paris.fr>
2022-01-17 19:06:55 +0100

    waiting for parsing success to send a quickresponse to siege

--- srcs/main_siege.cpp
+++ srcs/main_siege.cpp
@@ -15,17 +15,32 @@ int handleRequestBuffers(Server &server)
 	return 0;
 }
 
+bool
+quickparser(Buffer &buffer) // TODO debug purpose only, problem for big request
+{
+	for (unsigned long i = 0; i < buffer.getBuffer().size() - CRLFCRLF; i++)
+	{
+		if (isEndSection(buffer.getBuffer(), i))
+			return true;
+		else
+			buffer.getBuffer()[i] = 0;
+	}
+	return false;
+}
+
+#define CRLF_S "\r\n"
+
 void
 quickresponse(Server &server) //TODO copy, modify and integrate and delete after debug
 {
 	std::string response;
 	std::stringstream stream;
 	
-	stream << "HTTP/1.1 200 OK" << std::endl;
-	stream << "Content-Length: 2" << std::endl;
-	stream << "Content-Type: text/plain" << std::endl;
-	stream << "\r\n\r\n" << std::endl;
-	stream << "cc" << std::endl;
+	stream << "HTTP/1.1 200 OK" << CRLF_S;
+	stream << "Content-Length: 2" << CRLF_S;
+	stream << "Content-Type: text/plain" << CRLF_S;
+	stream << CRLF_S << CRLF_S;
+	stream << "cc" << CRLF_S;
 
 	stream >> response;
 
@@ -39,8 +54,15 @@ quickresponse(Server &server) //TODO copy, modify and integrate and delete after
 	for(; it != server.getRefConnections().end(); it++)
 	{	
 		Connection* connection = it->second;
+	//	connection->setPendingRequest(quickparser(connection->getInBuffer()));
+
+	//	if (connection->_pending_request 
+	//		&& server.isWritePossible(connection->getSocketFd()))
 		if (server.isWritePossible(connection->getSocketFd()))
+		{
 			write(connection->getSocketFd(), response.c_str(), 1000);
+			connection->setPendingRequest(false);
+		}
 	}
 }
 
@@ -48,9 +70,9 @@ int main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	struct timeval		tv;
-	std::vector<Server*> servers;
-	std::deque<RequestHandler> request_handlers;
+	struct timeval				tv;
+	std::vector<Server*>		servers;
+	std::deque<RequestHandler>	request_handlers;
 
 	Server::max_fd = 0;
 	Server::initFdset();
