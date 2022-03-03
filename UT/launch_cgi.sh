7f5ec681ac6e8afe35f7a693aeafbd280d7ee0b5
macrespo42 <maxime.crespopro@gmail.com>
2022-03-03 21:32:06 +0100

    cleanup multiple test directory

--- UT/launch_cgi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-echo "Aloha" \
-| env SCRIPT_NAME=index.php \
-		PATH_INFO=/home/daav/webserv/UT/index.php \
-		PATH_TRANSLATED=/home/daav/webserv/UT/index.php \
-		CONTENT_LENGTH=12 \
-		REQUEST_METHOD=POST \
-		REDIRECT_STATUS=200 \
-/usr/bin/php-cgi 
--- test/init.sh
+++
@@ -1,6 +0,0 @@
-service nginx start
-service ssh start
-while 1
-do
-	sleep 1
-done
